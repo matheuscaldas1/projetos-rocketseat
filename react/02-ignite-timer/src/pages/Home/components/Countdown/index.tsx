1c740fee32aa27aff750eaa435deb2a74fe97e36
Matheus Caldas <verossim.tv@gmail.com>
2023-07-20 09:38:41 -0300

    projeto 2 | modulo 5

--- react/02-ignite-timer/src/pages/Home/components/Countdown/index.tsx
+++ react/02-ignite-timer/src/pages/Home/components/Countdown/index.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useContext } from 'react'
 import { differenceInSeconds } from 'date-fns'
 import { CountdownContainer, Separator } from './styles'
-import { CyclesContext } from '../..'
+import { CyclesContext } from '../../../../contexts/CyclesContext'
 
 export function Countdown() {
   const {
--- react/02-ignite-timer/src/pages/Home/components/NewCycleForm/index.tsx
+++ react/02-ignite-timer/src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react'
-import { CyclesContext } from '../..'
 import { useFormContext } from 'react-hook-form'
+import { CyclesContext } from '../../../../contexts/CyclesContext'
 
 import { FormContainer, MinutesAmmountInput, TaskInput } from './styles'
 
@@ -31,7 +31,7 @@ export function NewCycleForm() {
         id="minutesAmount"
         placeholder="00"
         step={5}
-        min={1}
+        min={5}
         max={60}
         disabled={!!activeCycle}
         {...register('minutesAmount', { valueAsNumber: true })}
